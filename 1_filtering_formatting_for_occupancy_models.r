a58aa029fdc6384407151fd4c4f1448394dea2bf
f-duchenne <francois.duchenne@mnhn.fr>
2024-05-10 11:56:17 +0200

    new

---
+++ 1_filtering_formatting_for_occupancy_models.r
@@ -0,0 +1,17 @@
+###########################################
+#' Check for packages and if necessary install into library 
+#+ message = FALSE
+rm(list=ls())
+pkgs <- c("data.table", "dplyr","sparta") 
+
+inst <- pkgs %in% installed.packages()
+if (any(inst)) install.packages(pkgs[!inst])
+pkg_out <- lapply(pkgs, require, character.only = TRUE)
+
+# Loading data
+
+
+results <- dataDiagnostics(taxa = myData$taxa,
+                           site = myData$site,
+                           time_period = myData$time_period,
+                           progress_bar = FALSE)
