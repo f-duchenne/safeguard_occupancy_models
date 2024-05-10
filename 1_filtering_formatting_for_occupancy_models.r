930da5257535cac4ec68051cff8023002b1a548d
f-duchenne <francois.duchenne@mnhn.fr>
2024-05-10 11:59:00 +0200

    new

--- 1_filtering_formatting_for_occupancy_models.r
+++ 1_filtering_formatting_for_occupancy_models.r
@@ -8,8 +8,14 @@ inst <- pkgs %in% installed.packages()
 if (any(inst)) install.packages(pkgs[!inst])
 pkg_out <- lapply(pkgs, require, character.only = TRUE)
 
-# Loading data
+#defining working folder:
+setwd(dir="C:/Users/Duchenne/Documents/safeguard/data")
 
+# Loading data
+dat=readRDS("Safeguard_bee_df_final_v8_20240509.rds")
+
+#convert data.frame to data.table for more faster manipulations
+dat=as.data.table(dat)
 
 results <- dataDiagnostics(taxa = myData$taxa,
                            site = myData$site,
