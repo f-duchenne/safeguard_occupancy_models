a27536c67c2bccbdaa1737a48d8737f73d6ab169
f-duchenne <francois.duchenne@mnhn.fr>
2024-05-10 12:45:02 +0200

    new

--- 1_filtering_formatting_for_occupancy_models.r
+++ 1_filtering_formatting_for_occupancy_models.r
@@ -14,10 +14,19 @@ setwd(dir="C:/Users/Duchenne/Documents/safeguard/data")
 # Loading data
 dat=readRDS("Safeguard_bee_df_final_v8_20240509.rds")
 
-#convert data.frame to data.table for more faster manipulations
+#convert data.frame to data.table for more faster subsequent manipulations
 dat=as.data.table(dat)
 
-results <- dataDiagnostics(taxa = myData$taxa,
-                           site = myData$site,
-                           time_period = myData$time_period,
-                           progress_bar = FALSE)
+#define time periods
+dat[,time_period:=ifelse(.SD<=1980,"1980",ifelse(.SD<=1990,"1990",ifelse(.SD<=2000,"2000",ifelse(.SD<=2010,"2010","2020")))),.SDcols = "YEAR_2"]
+
+#roughly define sites (first approximation, sites should not be defined in WGS84 coordinates but using a coordinate system that conserve distances over latitude)
+dat$LATITUDE=as.numeric(dat$LATITUDE)
+dat$LONGITUDE=as.numeric(dat$LONGITUDE)
+dat[,site:=do.call(paste,do.call(round,list(x=.SD,digits=1))),.SDcols = which(names(dat) %in% c("LATITUDE","LONGITUDE"))]
+
+#use sparta tools to have a first look at the dataset
+results <- dataDiagnostics(taxa = dat$TAXON,
+                           site = dat$site,
+                           time_period = dat$time_period,
+                           progress_bar = TRUE)
